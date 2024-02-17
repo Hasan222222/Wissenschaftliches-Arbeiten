cc1046d86a6d2ea96f845e28a66deb4a29a0cae1
Hasan222222 <154332293+Hasan222222@users.noreply.github.com>
2024-02-17 12:15:59 +0100

    Add files via upload

---
+++ 8_Abd-Alkareem_Ryzhkovskii_Muenuesoglu.R
@@ -0,0 +1,14 @@
+#Nr.1
+#b)
+#Guetefunktion
+Wkeiten <- function(teta){
+  1-(dpois(53, lambda = teta) - dpois(28, lambda = teta))
+}
+
+wkeiten = sapply(1:100, Wkeiten)
+any(wkeiten < 0.05)
+
+#d)
+#Plot der Guetefunktion
+plot(1:100, wkeiten, type = "l", xlab = expression("teta"), ylab = "Wkeitswerte",
+     main = "Plot zur Guetefunktion")
